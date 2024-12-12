624d9fd862b0ebbdadbc8a532c3783327a771231
SUFIYAN <155263639+muhammadsufiyanattari@users.noreply.github.com>
2024-12-12 10:17:46 +0500

    add theme card total

--- my-project/src/Pages/CardTotal.jsx
+++ my-project/src/Pages/CardTotal.jsx
@@ -21,7 +21,7 @@ function CardTotal() {
         {/* Table Section */}
         <div className="w-full max-w-6xl  justify-center   h-[356px] rounded sm:hidden mobile:hidden 2xl:flex xl:flex lg:flex md:flex  flex-col gap-6 p-6 shadow-md">
           {/* Table Header */}
-          <div className="hidden z-10  md:grid grid-cols-4 gap-4 bg-slate-100 p-4 rounded text-center md:text-left font-semibold text-gray-700">
+          <div className={`${darkMode?"darkTheme text-white":"text-black"} hidden z-10  md:grid grid-cols-4 gap-4 bg-slate-100 p-4 rounded text-center md:text-left font-semibold `}>
             <div>Product</div>
             <div>Price</div>
             <div>Quantity</div>
@@ -31,18 +31,18 @@ function CardTotal() {
           {/* Table Body */}
           <div className="space-y-4 md:space-y-0  h-auto overflow-y-auto flex flex-col gap-6 ">
             {/* First Product Row */}
-            <div className="flex  mobile:items-center flex-col md:grid grid-cols-4 gap-4 bg-slate-100 p-4 rounded">
-              <div className="flex sm:justify-center mobile:justify-center items-center md:gap-2 gap-4">
+            <div className={`${darkMode?"darkTheme text-white":""} flex  mobile:items-center flex-col md:grid grid-cols-4 gap-4 bg-slate-100 p-4 rounded`}>
+              <div className={`${darkMode?"":""} flex sm:justify-center mobile:justify-center items-center md:gap-2 gap-4`}>
                 <img className="w-16 h-12" src={led} alt="LCD Monitor" />
-                <p className="text-gray-700 md:text-xs md:font-normal font-medium">
+                <p className={`${darkMode?"text-white":""} text-gray-700 md:text-xs md:font-normal font-medium`}>
                   LCD Monitor
                 </p>
               </div>
-              <div className="text-gray-700 text-center md:text-left flex items-center">
+              <div className={`${darkMode?"text-white":""} text-gray-700 text-center md:text-left flex items-center`}>
                 $650
               </div>
               <div className="text-center flex items-center">
-                <select className="border border-gray-300 rounded w-20 p-2">
+                <select className="border border-gray-300 rounded w-20 p-2 text-black" >
                   {[...Array(10)].map((_, i) => (
                     <option key={i} value={i + 1}>
                       {i + 1}
@@ -50,20 +50,20 @@ function CardTotal() {
                   ))}
                 </select>
               </div>
-              <div className="text-gray-700 text-center md:text-left  flex items-center ">
+              <div className={`${darkMode?"text-white":""} text-gray-700 text-center md:text-left  flex items-center `}>
                 $650
               </div>
             </div>
 
             {/* Second Product Row */}
-            <div className="flex flex-col mobile:items-center md:grid grid-cols-4 gap-4 bg-slate-100 p-4 rounded">
+            <div className={`${darkMode?" darkTheme":""} flex flex-col mobile:items-center md:grid grid-cols-4 gap-4 bg-slate-100 p-4 rounded`}>
               <div className="flex items-center md:gap-2 gap-4 sm:justify-center mobile:justify-center">
                 <img className="w-16 h-12" src={game} alt="Gamepad" />
-                <p className="text-gray-700 md:text-xs md:font-normal font-medium">
+                <p className={`${darkMode?"text-white":""} text-gray-700 md:text-xs md:font-normal font-medium`}>
                   HI Gamepad
                 </p>
               </div>
-              <div className="text-gray-700 text-center md:text-left">$650</div>
+              <div className={`${darkMode?"text-white":""} text-gray-700 text-center md:text-left`}>$650</div>
               <div className="text-center flex items-center ">
                 <select className="border border-gray-300 rounded w-20 p-2">
                   {[...Array(10)].map((_, i) => (
@@ -73,7 +73,7 @@ function CardTotal() {
                   ))}
                 </select>
               </div>
-              <div className="text-gray-700 text-center md:text-left">$650</div>
+              <div className={`${darkMode?"text-white":""} text-gray-700 text-center md:text-left`}>$650</div>
             </div>
           </div>
         </div>
@@ -146,10 +146,10 @@ function CardTotal() {
 
         {/* Buttons */}
         <div className="w-full xl:flex-row md:flex-row  2xl:flex-row lg:flex-row max-w-6xl flex sm:flex-col mobile:flex-col   justify-between mt-">
-          <button className="px-6  2xl:w-[218px] 2xl:h-[56px] xl:w-[218px] xl:h-[56px] lg:w-[218px] lg:h-[56px] md:w-[218px] md:h-[56px]  py-3 text-base sm:my-2 mobile:my-2 bg-white active:bg-red-700 active:text-white text-black font-medium border border-black rounded  ">
+          <button className={`${darkMode?"bg-black text-white":"bg-white text-black"} px-6  2xl:w-[218px] 2xl:h-[56px] xl:w-[218px] xl:h-[56px] lg:w-[218px] lg:h-[56px] md:w-[218px] md:h-[56px]  py-3 text-base sm:my-2 mobile:my-2  active:bg-red-700 active:text-white  font-medium border border-black rounded  `}>
             Return To Shop
           </button>
-          <button className="px-6  2xl:w-[218px] 2xl:h-[56px] xl:w-[218px] xl:h-[56px] lg:w-[218px] lg:h-[56px] md:w-[218px] md:h-[56px]  py-3 text-base sm:my-2 mobile:my-2 bg-white active:bg-red-700 active:text-white text-black font-medium border border-black rounded ">
+          <button className={`${darkMode?"bg-black text-white":"bg-white text-black"} px-6  2xl:w-[218px] 2xl:h-[56px] xl:w-[218px] xl:h-[56px] lg:w-[218px] lg:h-[56px] md:w-[218px] md:h-[56px]  py-3 text-base sm:my-2 mobile:my-2  active:bg-red-700 active:text-white  font-medium border border-black rounded `}>
             Update Cart
           </button>
         </div>
@@ -171,9 +171,9 @@ function CardTotal() {
           </div>
 
           {/* Total Section */}
-          <div className="lg:w-1/3 border border-gray-400 p-6 rounded shadow-md bg-white">
+          <div className={`${darkMode?"bg-black text-white":" bg-white"} lg:w-1/3 border border-gray-400 p-6 rounded shadow-md `}>
             <h2 className="font-bold text-xl mb-4">Cart Total</h2>
-            <div className="space-y-4 text-gray-700">
+            <div className={`${darkMode?"bg-black text-white":"bg-white text-black"} space-y-4 text-gray-700`}>
               <div className="flex justify-between border-b pb-2">
                 <span>Subtotal:</span>
                 <span>$1750</span>
