ac39e49fb1e8bfa3c3835d8823003f807531a0f2
SUFIYAN <155263639+muhammadsufiyanattari@users.noreply.github.com>
2024-12-16 11:30:35 +0500

    add navbar small screen animation

--- my-project/src/components/Navbar.jsx
+++ my-project/src/components/Navbar.jsx
@@ -16,6 +16,28 @@ import { useDispatch, useSelector } from "react-redux";
 import { toggleDarkMode } from "../store/darkModeSlice";
 import { IoSunny } from "react-icons/io5";
 import { IoMoon } from "react-icons/io5";
+import AOS from 'aos';
+import 'aos/dist/aos.css'; // You can also use <link> for styles
+// ..
+AOS.init({disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+  startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+  initClassName: 'aos-init', // class applied after initialization
+  animatedClassName: 'aos-animate', // class applied on animation
+  useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+  disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+  debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+  throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+  
+
+  // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+  offset: 120, // offset (in px) from the original trigger point
+  delay: 0, // values from 0 to 3000, with step 50ms
+  duration: 400, // values from 0 to 3000, with step 50ms
+  easing: 'ease', // default easing for AOS animations
+  once: false, // whether animation should happen only once - while scrolling down
+  mirror: false, // whether elements should animate out while scrolling past them
+  anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+});
 
 // ../assets/logo/'
 let Mylink = [
@@ -133,7 +155,7 @@ export default function MyNavbar() {
         {/* sm screen */}
 
         {menu ? (
-          <div className="link top-[70px] bg-white w-[100%] z-10   lg:hidden xl:hidden 2xl:hidden absolute">
+          <div   data-aos="fade-right" className="link top-[70px] bg-white w-[100%] z-10   lg:hidden xl:hidden 2xl:hidden absolute">
             <ul className="flex pl-8 flex-col gap-5">
               {Mylink.map((value, index) => {
                 return (
