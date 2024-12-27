fe6e65933861883128c17f7ae8666169a55fd646
SUFIYAN <155263639+muhammadsufiyanattari@users.noreply.github.com>
2024-12-27 23:54:58 +0500

    update but i dont no

--- my-project/src/components/Slider.jsx
+++ my-project/src/components/Slider.jsx
@@ -254,12 +254,10 @@ export default () => {
           <div className="flex justify-center items-center   ">
             {isLoading ? (
               <div className="font-extrabold flex-wrap justify-center items-center flex gap-10 text-4xl">
-                              <SekeletonCart/>
-                              <SekeletonCart/>
-                              <SekeletonCart/>
-                              <SekeletonCart/>
-                              
-
+                <SekeletonCart />
+                <SekeletonCart />
+                <SekeletonCart />
+                <SekeletonCart />
               </div>
             ) : null}
             {error}
